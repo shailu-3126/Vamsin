4a911d667a634dc66b07de76621888a9c7c6d050
nallabothushailaja <nshailu2003@gmail.com>
2025-10-15 19:21:22 +0530

    Fix Flake8 E302 linting errors

--- vamsins.py
+++ vamsins.py
@@ -1,5 +1,15 @@
+# vamsins.py
+
 from fastapi import FastAPI
+
 app = FastAPI()
+
+
 @app.get("/")
 def read_root():
     return {"message": "Hello from Vamsin App!"}
+
+
+@app.get("/health")
+def health_check():
+    return {"status": "OK"}
